d24c1a36367e3a58fc1d91c70bd4f33e346943ed
DiegoMoralesBravo <Diegomorales1359@gmail.com>
2022-02-27 08:56:38 -0600

    Se esconde el key

--- awsproject/settings.py
+++ awsproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uo_x7g4z%@*i0vowsh_a3x#k^o*40+3tamy(en!fd9cg+z_#u^'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
